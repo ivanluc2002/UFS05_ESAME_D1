6be29c51016bda04b7332c57e982642f2cb44197
Ivanluc2002 <i.lucchini@itsrizzoli.it>
2022-05-23 14:37:37 +0200

    feat wine

--- src/main/java/org/example/Wine.java
+++ src/main/java/org/example/Wine.java
@@ -1,20 +1,28 @@
 package org.example;
 
 public class Wine {
-    private String id ;
+    private String id;
     private String name;
     private double price;
     private String type;
 
 
-
-    public Wine(String name, String surname) {
+    public Wine(String id, String name, double price, String type) {
+        this.id = id;
         this.name = name;
-        this.surname = surname;
+        this.price = price;
+        this.type = type;
     }
 
+    public String getId() {
+        return id;
+    }
 
-    public  String getName() {
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
     }
 
@@ -22,11 +30,19 @@ public class Wine {
         this.name = name;
     }
 
-    public String getSurname() {
-        return surname;
+    public double getPrice() {
+        return price;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
