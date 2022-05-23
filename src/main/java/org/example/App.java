b5132caa8fff33d76bfc52d4549c4f2464043965
Ivanluc2002 <i.lucchini@itsrizzoli.it>
2022-05-23 14:47:27 +0200

    feat wine

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,7 @@ import com.sun.net.httpserver.HttpServer;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 
 public class App
 {
@@ -22,6 +23,27 @@ public class App
         server.setExecutor(null); // creates a default executor
         server.start();
 
+            String jsonStr = "[ {\"id\": \"13\",\n" +
+                    " \"name\": \"Dom perignon Vintage Moet &chandon 2008\"\n" +
+                    "\"price\": \"225,94\"\n"+
+                    "\"type\": \"white\"\n"+
+                    " },\n" +
+                    "\"id\": \"14\",\n" +
+                " \"name\": \"Pignoli\"\n" +
+                "\"price\": \"133,0\"\n"+
+                "\"type\": \"red\"\n"+
+                " },\n" +
+                    "\"id\": \"124\",\n" +
+                " \"name\": \"pinot nero\"\n" +
+                "\"price\": \"43\"\n"+
+                "\"type\": \"red\"\n"+
+                " },\n"+
+                    "]\n";
+
+            Gson gson = new Gson();
+            Wine[] vino = gson.fromJson( jsonStr, Wine[].class);
+            System.out.println(Arrays.toString(vino));
+        }
+
 
     }
-}
